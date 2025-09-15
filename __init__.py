d9b7adeb5b00086e3f0fa0c009f067657bb26fe1
neo <9028430+neo@users.noreply.github.com>
2025-09-15 16:01:00 -0400

    Use black formatter

--- __init__.py
+++ __init__.py
@@ -21,8 +21,16 @@ class EmptyLatentImageQwen:
     @classmethod
     def INPUT_TYPES(s):
         dimensions = list(s.SUPPORTED_DIMENSIONS)
-        return {"required": { "aspect_ratio": (dimensions, {"default": dimensions[len(dimensions) // 2]}),
-                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}) }}
+        return {
+            "required": {
+                "aspect_ratio": (
+                    dimensions,
+                    {"default": dimensions[len(dimensions) // 2]},
+                ),
+                "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
+            }
+        }
+
     RETURN_TYPES = ("LATENT",)
     FUNCTION = "generate"
 
@@ -30,18 +38,20 @@ class EmptyLatentImageQwen:
 
     def generate(self, aspect_ratio, batch_size=1):
         width, height = self.SUPPORTED_DIMENSIONS[aspect_ratio]
-        latent = torch.zeros([batch_size, 4, height // 8, width // 8], device=self.device)
-        return ({"samples":latent}, )
-        
+        latent = torch.zeros(
+            [batch_size, 4, height // 8, width // 8], device=self.device
+        )
+        return ({"samples": latent},)
+
 
 class EmptyLatentImageSDXL:
     # https://platform.stability.ai/docs/features/api-parameters#about-dimensions
     SUPPORTED_DIMENSIONS = {
-        "12:5 (1536 x 640)": (1536, 640), # 2.4
-        "7:4 (1344 x 768)": (1344, 768), # 1.75
-        "19:13 (1216 x 832)": (1216, 832), # 1.4615
-        "9:7 (1152 x 896)": (1152, 896), # 1.2857
-        "1:1 (1024 x 1024)": (1024, 1024), # 1
+        "12:5 (1536 x 640)": (1536, 640),  # 2.4
+        "7:4 (1344 x 768)": (1344, 768),  # 1.75
+        "19:13 (1216 x 832)": (1216, 832),  # 1.4615
+        "9:7 (1152 x 896)": (1152, 896),  # 1.2857
+        "1:1 (1024 x 1024)": (1024, 1024),  # 1
         "7:9 (896 x 1152)": (896, 1152),
         "13:19 (832 x 1216)": (832, 1216),
         "4:7 (768 x 1344)": (768, 1344),
@@ -54,8 +64,16 @@ class EmptyLatentImageSDXL:
     @classmethod
     def INPUT_TYPES(s):
         dimensions = list(s.SUPPORTED_DIMENSIONS)
-        return {"required": { "aspect_ratio": (dimensions, {"default": dimensions[len(dimensions) // 2]}),
-                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}) }}
+        return {
+            "required": {
+                "aspect_ratio": (
+                    dimensions,
+                    {"default": dimensions[len(dimensions) // 2]},
+                ),
+                "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
+            }
+        }
+
     RETURN_TYPES = ("LATENT",)
     FUNCTION = "generate"
 
@@ -63,21 +81,58 @@ class EmptyLatentImageSDXL:
 
     def generate(self, aspect_ratio, batch_size=1):
         width, height = self.SUPPORTED_DIMENSIONS[aspect_ratio]
-        latent = torch.zeros([batch_size, 16, height // 8, width // 8], device=self.device)
-        return ({"samples":latent}, )
+        latent = torch.zeros(
+            [batch_size, 16, height // 8, width // 8], device=self.device
+        )
+        return ({"samples": latent},)
 
 
 class ToggleDifferentStepsCfgSet:
-    
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "steps": ("INT", {"default": 20, "min": 1, "max": 10000, "tooltip": "The number of steps used in the denoising process."}),
-                "cfg": ("FLOAT", {"default": 2.5, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01, "tooltip": "The Classifier-Free Guidance scale balances creativity and adherence to the prompt. Higher values result in images more closely matching the prompt however too high values will negatively impact quality."}),
+                "steps": (
+                    "INT",
+                    {
+                        "default": 20,
+                        "min": 1,
+                        "max": 10000,
+                        "tooltip": "The number of steps used in the denoising process.",
+                    },
+                ),
+                "cfg": (
+                    "FLOAT",
+                    {
+                        "default": 2.5,
+                        "min": 0.0,
+                        "max": 100.0,
+                        "step": 0.1,
+                        "round": 0.01,
+                        "tooltip": "The Classifier-Free Guidance scale balances creativity and adherence to the prompt. Higher values result in images more closely matching the prompt however too high values will negatively impact quality.",
+                    },
+                ),
                 "use_the_other_set": ("BOOLEAN", {"default": False}),
-                "the_other_steps": ("INT", {"default": 4, "min": 1, "max": 10000, "tooltip": "The number of steps used in the denoising process."}),
-                "the_other_cfg": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01, "tooltip": "The Classifier-Free Guidance scale balances creativity and adherence to the prompt. Higher values result in images more closely matching the prompt however too high values will negatively impact quality."}),
+                "the_other_steps": (
+                    "INT",
+                    {
+                        "default": 4,
+                        "min": 1,
+                        "max": 10000,
+                        "tooltip": "The number of steps used in the denoising process.",
+                    },
+                ),
+                "the_other_cfg": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": 0.0,
+                        "max": 100.0,
+                        "step": 0.1,
+                        "round": 0.01,
+                        "tooltip": "The Classifier-Free Guidance scale balances creativity and adherence to the prompt. Higher values result in images more closely matching the prompt however too high values will negatively impact quality.",
+                    },
+                ),
             }
         }
 
@@ -87,15 +142,22 @@ class ToggleDifferentStepsCfgSet:
     CATEGORY = "utils"
 
     def choose(self, steps, cfg, use_the_other_set, the_other_steps, the_other_cfg):
-        return (steps, cfg) if not use_the_other_set else (the_other_steps, the_other_cfg)
+        return (
+            (steps, cfg) if not use_the_other_set else (the_other_steps, the_other_cfg)
+        )
 
 
 class ToggleDifferentText:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"text": ("STRING", {"multiline": True, "forceInput": True}),
-                             "use_different_text": ("BOOLEAN", {"default": True}),
-                             "different_text": ("STRING", {"multiline": True}),}}
+        return {
+            "required": {
+                "text": ("STRING", {"multiline": True, "forceInput": True}),
+                "use_different_text": ("BOOLEAN", {"default": True}),
+                "different_text": ("STRING", {"multiline": True}),
+            }
+        }
+
     RETURN_TYPES = ("STRING",)
     FUNCTION = "choose"
 
@@ -104,9 +166,10 @@ class ToggleDifferentText:
     def choose(self, text, use_different_text, different_text):
         return text if not use_different_text else different_text
 
+
 NODE_CLASS_MAPPINGS = {
-  'Empty Latent Image (Qwen)': EmptyLatentImageQwen,
-  'Empty Latent Image (SDXL)': EmptyLatentImageSDXL,
-  'Toggle Different Steps/CFG Set': ToggleDifferentStepsCfgSet,
-  'Toggle Different Text': ToggleDifferentText,
+    "Empty Latent Image (Qwen)": EmptyLatentImageQwen,
+    "Empty Latent Image (SDXL)": EmptyLatentImageSDXL,
+    "Toggle Different Steps/CFG Set": ToggleDifferentStepsCfgSet,
+    "Toggle Different Text": ToggleDifferentText,
 }
